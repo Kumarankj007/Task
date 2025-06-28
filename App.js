b5336aa544f289d9afdedbe4b52a99e3bb98b292
Kumarankj007 <153486963+Kumarankj007@users.noreply.github.com>
2025-06-28 12:36:01 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,20 +3,9 @@ import {Text} from 'react-native';
 import {AuthStack} from './src/Nav/Stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import {useDispatch, useSelector} from 'react-redux';
-import {setCart} from './src/redux/CartSlice';
 
 export default function App() {
-  const dispatch = useDispatch();
-  const {cart, subTotal} = useSelector(state => state.cart);
-
-  useEffect(() => {
-    // dispatch(setCart([]));
-  }, []);
   
-  useEffect(() => {
-    // console.log(cart, 'cart', subTotal);
-  }, [cart, subTotal]);
   return (
     <NavigationContainer>
       <GestureHandlerRootView>
